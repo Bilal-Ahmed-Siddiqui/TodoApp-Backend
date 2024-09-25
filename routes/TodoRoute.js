59a83a69e38bd6eaca215cfd78249945ec6d4a53
Bilal Ahmed <bilalkietacc@gmail.com>
2024-09-25 07:00:39 +0500

    Todo CRUD End Points completed

--- routes/TodoRoute.js
+++ routes/TodoRoute.js
@@ -7,22 +7,68 @@ router.post("/create", async (req, res) => {
   try {
     const { title, description } = req.body;
 
-    
     const newTodo = new Todo({
       title,
       description,
     });
 
     const savedTodo = await newTodo.save();
-    res.status(201).json(savedTodo)
+    res.status(201).json(savedTodo);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
 //get request
-router.get("/", (req, res) => {
-  res.send("todo route working");
+router.get("/", async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.status(200).json(todos);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
+
+//update request
+router.put("/update/:id", async (req, res) => {
+  try {
+    const { title, description, isCompleted } = req.body;
+    const { id } = req.params;
+
+    const updatedTodo = await Todo.findByIdAndUpdate(
+      id,
+      { title, description, isCompleted },
+      { new: true }
+    );
+
+    if (!updatedTodo) {
+      res.status(404).json({ error: "todo Not Found" });
+    } else {
+      res.status(201).json(updatedTodo);
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+//delete request
+router.delete("/delete/:id", async (req, res)=>{
+  try {
+    const { id } = req.params;
+    
+    const deletedTodo = await Todo.findByIdAndDelete(id);
+    
+    if(!deletedTodo){
+      res.status(404).json({ error: "todo Not Found" });
+    }
+    else{
+      res.status(201).json({Success:"Todo deleted successfully"});
+    }
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+})
 
 module.exports = router;
