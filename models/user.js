8426f3acb4678c5c329f0108898d6b955854dd34
Bilal Ahmed <bilalkietacc@gmail.com>
2024-09-28 06:32:02 +0500

    Todo Routes validation thorough Express Validator

--- models/user.js
+++ models/user.js
@@ -1,22 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema(
-    {
-        username: {
-            type: "string",
-            required: true
-        },
-        email: {
-            type: "string",
-            required: true,
-            unique: true
-        },
-        password: {
-            type: "string",
-            required: true
-        }
-    }
-)
+const userSchema = new mongoose.Schema({
+  username: {
+    type: "string",
+    required: true,
+  },
+  email: {
+    type: "string",
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: "string",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model("User", userSchema);
