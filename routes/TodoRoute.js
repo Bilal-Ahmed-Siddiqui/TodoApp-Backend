8d80249cee3b252ba004eb452e2dc97bd3ca5818
bilal Ahmed <bilalkietacc@outlook.com>
2024-09-28 06:32:02 +0500

    Todo Routes validation thorough Express Validator

--- routes/TodoRoute.js
+++ routes/TodoRoute.js
@@ -1,23 +1,47 @@
 const express = require("express");
 const Todo = require("../models/Todo");
+const { body, validationResult } = require("express-validator");
 const router = express.Router();
 
 //post request
-router.post("/create", async (req, res) => {
-  try {
-    const { title, description } = req.body;
+router.post(
+  "/create",
+  [
+    body("title")
+      .isString()
+      .isLength({ max: 50 })
+      .withMessage("Title is required"),
+    body("description")
+      .optional()
+      .isString()
+      .isLength({ max: 200 })
+      .withMessage(
+        "Description must be a string and cannot exceed 500 characters"
+      ),
+  ],
+  async (req, res) => {
+    //express validation check
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      return res.status(400).json({ error: error });
+    }
 
-    const newTodo = new Todo({
-      title,
-      description,
-    });
+    //end point main logic
+    try {
+      const { title, description } = req.body;
 
-    const savedTodo = await newTodo.save();
-    res.status(201).json(savedTodo);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+      const newTodo = new Todo({
+        title,
+        description,
+      });
+
+      const savedTodo = await newTodo.save();
+      res.status(201).json(savedTodo);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
   }
-});
+);
 
 //get request
 router.get("/", async (req, res) => {
@@ -30,45 +54,74 @@ router.get("/", async (req, res) => {
 });
 
 //update request
-router.put("/update/:id", async (req, res) => {
-  try {
-    const { title, description, isCompleted } = req.body;
-    const { id } = req.params;
-
-    const updatedTodo = await Todo.findByIdAndUpdate(
-      id,
-      { title, description, isCompleted },
-      { new: true }
-    );
-
-    if (!updatedTodo) {
-      res.status(404).json({ error: "todo Not Found" });
-    } else {
-      res.status(201).json(updatedTodo);
+router.put(
+  "/update/:id",
+  [
+    body("title")
+      .optional()
+      .isString()
+      .isLength({ min: 50 })
+      .withMessage("Title must be a non-empty string"),
+    body("description")
+      .optional()
+      .isString()
+      .isLength({ max: 200 })
+      .withMessage(
+        "Description must be a string and cannot exceed 500 characters"
+      ),
+    body("isCompleted")
+      .optional()
+      .isBoolean()
+      .withMessage("isCompleted must be a boolean"),
+  ],
+  async (req, res) => {
+    //express validation check
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
 
+    //end point main logic
+    try {
+      const { title, description, isCompleted } = req.body;
+      const { id } = req.params;
+
+      const updatedTodo = await Todo.findByIdAndUpdate(
+        id,
+        { title, description, isCompleted },
+        { new: true }
+      );
+
+      if (!updatedTodo) {
+        res.status(404).json({ error: "todo Not Found" });
+      } else {
+        res.status(201).json(updatedTodo);
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+);
 
 //delete request
-router.delete("/delete/:id", async (req, res)=>{
-  try {
-    const { id } = req.params;
-    
-    const deletedTodo = await Todo.findByIdAndDelete(id);
-    
-    if(!deletedTodo){
-      res.status(404).json({ error: "todo Not Found" });
-    }
-    else{
-      res.status(201).json({Success:"Todo deleted successfully"});
-    }
+router.delete(
+  "/delete/:id",
+  async (req, res) => {
+    //end point main logic
+    try {
+      const { id } = req.params;
 
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+      const deletedTodo = await Todo.findByIdAndDelete(id);
+
+      if (!deletedTodo) {
+        res.status(404).json({ error: "todo Not Found" });
+      } else {
+        res.status(201).json({ Success: "Todo deleted successfully" });
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
   }
-})
+);
 
 module.exports = router;
