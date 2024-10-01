1e95907442bebc1cc8e4d4984d4cf9db8a41c6db
Bilal Ahmed <bilalkietacc@gmail.com>
2024-10-01 20:57:24 +0500

    user login endpoint added

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -2,9 +2,11 @@ const express = require("express");
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
-
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 
+
+const JWT_SECRET = "Mysecret"
 //signup route
 router.post(
   "/signup",
@@ -51,6 +53,39 @@ router.post(
 );
 
 
-//test commit command
+//user login endpoint 
+router.post("/login",
+[
+  body("email").exists(),
+  body("password").exists()
+]
+,async(req, res)=>{
+  //express validator logic
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    return res.status(400).json({ error: error });
+  }
+  //end point main logic
+  try {
+    const {email, password} = req.body
+
+    const userExist = await User.findOne({email});
+    if(!userExist){
+      return res.status(400).json({ message: "invalid email or password" });
+    }
+
+    const correctPass = await bcrypt.compare(password,userExist.password)
+    if(!correctPass){
+      return res.status(400).json({ message: "invalid email or password" });
+    }
+
+    const token = await jwt.sign({id: userExist._id}, JWT_SECRET)
+
+    res.status(200).json({token: token});
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+})
 
 module.exports = router;
