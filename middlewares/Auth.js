fd8806ba1289710d73217abfe82a32cb4b5cf88c
Bilal Ahmed <bilalkietacc@gmail.com>
2024-10-03 20:57:17 +0500

    auth middleware created

---
+++ middlewares/Auth.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken")
+
+const JWT_SECRET = "Mysecret"
+
+const authMiddleware = (req, res, next) => {
+    const token = req.header("token");
+
+    if(!token){
+        return res.status(401).json({error: "token not present"})
+    }
+
+    try {
+        const decodedToken = jwt.verify(token,JWT_SECRET);
+        req.user = decodedToken.user;
+        next();
+    } catch (error) {
+        return res.status(401).json({error: "invalid token"})
+    }
+}
+
+module.exports = authMiddleware
