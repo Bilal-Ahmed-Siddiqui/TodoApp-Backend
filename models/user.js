cd8fe3be088a12f37c37f1a73ed38f9e9a9de625
Bilal Ahmed <bilalkietacc@gmail.com>
2024-10-12 20:03:11 +0500

    multer integration

--- models/user.js
+++ models/user.js
@@ -1,24 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema(
-    {
-        username: {
-            type: String,
-            required: true
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true
-        },
-        password: {
-            type: String,
-            required: true
-        }
-    }
-)
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  profilePicture: {
+    type: String,
+  },
+});
 
+const User = mongoose.model("User", userSchema);
 
-const User = mongoose.model("User", userSchema)
-
-module.exports = User
+module.exports = User;
