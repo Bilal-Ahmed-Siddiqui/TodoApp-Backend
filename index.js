3f2489f6176b5ada7540598118500a5cb29426c4
bilal Ahmed <bilalkietacc@outlook.com>
2024-11-01 07:25:00 +0500

    vercel configurations

--- index.js
+++ index.js
@@ -4,19 +4,16 @@ const cors = require("cors");
 const TodoRoute = require("./routes/TodoRoute");
 const UserRoute = require("./routes/UserRoute");
 require("dotenv").config();
+
 const app = express();
 
-localURL = "mongodb://localhost:27017/Backend";
-AtlasURL = `mongodb+srv://bilalahmed:${process.env.DB_PASSWORD}@forteaching.plyfh.mongodb.net/?retryWrites=true&w=majority&appName=forTeaching`;
+const AtlasURL = `mongodb+srv://bilalahmed:${process.env.DB_PASSWORD}@forteaching.plyfh.mongodb.net/?retryWrites=true&w=majority&appName=forTeaching`;
 
 app.use(express.json());
 app.use(cors());
 
 mongoose
-  .connect(AtlasURL, {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
-  })
+  .connect(AtlasURL)
   .then(() => {
     console.log("database is connected");
   })
@@ -25,15 +22,12 @@ mongoose
   });
 
 app.get("/", (req, res) => {
-  // console.log("someone sent a request")
   res.send("Working");
 });
 
-//routes
-
+// Routes
 app.use("/todo", TodoRoute);
 app.use("/user", UserRoute);
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
-});
+// Export the app for Vercel
+module.exports = app;
